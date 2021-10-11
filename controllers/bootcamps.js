da924a705d18b146e68f7260f58bb042f17ffabc
PVNCCDSB\pkellar <pkellar@pvnccdsb.on.ca>
2021-10-11 13:35:26 -0400

    added advance query options for /bootcamps route, added Course schema

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,10 +7,74 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+  //Copy of Requust Query
+  const reqQuery = { ...req.query };
+
+  // Fields to Exclude
+  const removeFields = ['select', 'sort', 'limit', 'page'];
+
+  // Loop over removeFields and delete from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  // Creating a query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create proper operators for Mongo ($gt, $lte, etc)
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => {
+    return `$${match}`;
+  });
+
+  // Finding the resource
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Pageination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  //Execute Query
+  const bootcamps = await query;
+
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination,
     data: bootcamps,
   });
 });
